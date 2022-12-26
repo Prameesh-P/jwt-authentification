39338c90290fbee9aa8c58ac492ff8b6f551b3bd
Prameesh <prameepramee0@gmail.com>
2022-12-27 03:26:20 +0530

    comments on main.go file

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"net/http"
 )
 
+// starting main func
 func main() {
+	//three handlers
 	http.HandleFunc("/login", Login)
 	http.HandleFunc("/home", Home)
 	http.HandleFunc("/refresh", Refresh)
+	//starting port
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
