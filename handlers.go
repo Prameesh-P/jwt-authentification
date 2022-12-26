f0de309f304880db42dc5a898d6f48e210564f82
Prameesh <prameepramee0@gmail.com>
2022-12-27 03:45:55 +0530

    handler comments updated

--- handlers.go
+++ handlers.go
@@ -8,47 +8,66 @@ import (
 )
 
 var JwtKey = []byte("secret_key")
+
+// Users this just a sample to store username and password.You can use the help of database.
+
 var Users = map[string]string{
 	"Pramee":   "123",
 	"Prameesh": "12345",
 }
 
+//Credentials for check api request data..
+
 type Credentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+
 type Claims struct {
 	Username string `json:"username"`
-	jwt.StandardClaims
+	jwt.StandardClaims //Inside this have lot of preset value.Now I am using Expire At keyword.
 }
 
+// Home this is a home handler
 func Home(w http.ResponseWriter, r *http.Request) {
 
 }
+// Login this is a login handler
 func Login(w http.ResponseWriter, r *http.Request) {
+	// here we are create the object of the Credentials struct.
 	var credentials Credentials
+	//and decode that object
 	err := json.NewDecoder(r.Body).Decode(&credentials)
+	// if any error handle.
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		//Nothing on the credential return to badRequest
 		return
 	}
-	exepectedPassword, ok := Users[credentials.Username]
-	if !ok || exepectedPassword != credentials.Password {
-		w.WriteHeader(http.StatusBadRequest)
+	//if anything on that credential we want check our expected password and credential password is same.
+
+	expectedPassword, ok := Users[credentials.Username] //if you get actual password ok is true.
+														//else ok is false
+	if !ok || expectedPassword != credentials.Password {// not ok(!ok) means our expected password is wrong
+
+		w.WriteHeader(http.StatusBadRequest)			// and return to badRequest
 		return
 	}
+	//everything is ok we are creating an expiration time
 	expirationTime := time.Now().Add(time.Minute * 5)
+	//and we are updating Claims variable
 	claims := Claims{
 		Username: credentials.Username,
 		StandardClaims:jwt.StandardClaims{ExpiresAt: expirationTime.Unix()
 		},
 	}
-	token:=jwt.NewWithClaims(jwt.SigningMethodES256,claims)
+	token:=jwt.NewWithClaims(jwt.SigningMethodES256,claims)//and finally we are passing signing method.
 	tokenString,err:=token.SignedString(JwtKey)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
-	}
+	}//and setting our cookie
 	http.SetCookie(w,&http.Cookie{
 		Name: "token",
 		Value: tokenString,
